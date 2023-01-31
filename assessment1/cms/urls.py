50d1f5e11489f3e7f2354c67796164e05a867d13
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 <marboynel@gmail.com>
2023-01-31 21:42:24 +1300

    added register, login

---
+++ assessment1/cms/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+# from .views import 
+
+urlpatterns = [
+    path ('', views.home, name = 'home'),
+    path ('login/', views.login_user, name = 'login'),
+    path ('register/', views.register_user, name = 'register'),
+
+]
